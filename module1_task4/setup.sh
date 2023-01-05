13262b25cc3f0886436409687be63bf4e8278797
gasstin <gastonl1994@gmail.com>
2023-01-05 10:59:02 -0800

    fixing setup.sh install hugo

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,10 +1,10 @@
 sudo apt-get update 
 url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
-curl -s $url -L -o hugo_latest.tar.gz
-tar -zxf hugo_latest.tar.gz -C /usr/local/bin
+curl -s $url -L -o hugo.tar.gz
+tar -zxf hugo.tar.gz -C /usr/local/bin
 rm /usr/local/bin/README.md
 rm /usr/local/bin/LICENSE
-rm hugo_latest.tar.gz
+rm hugo.tar.gz
 apt-get install -y make
 make build
 exit 0
