d42f34c0b1f89807fde9d3a118f5f37f10967683
gasstin <gastonl1994@gmail.com>
2023-01-05 07:41:09 -0800

    fixing setup

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
 docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
 exit
-apt-get update && apt-get install -y hugo make
+sudo apt-get update && sudo apt-get install -y hugo make
 make build
