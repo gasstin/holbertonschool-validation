e1ea8eabef8966217c002803a3eea9f62c4af1ac
gasstin <gastonl1994@gmail.com>
2023-01-05 10:51:30 -0800

    fixing setup.sh install hugo

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update 
-url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_macOS-64bit.tar.gz')
+url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
 curl -s $url -L -o hugo_latest.tar.gz
 tar -zxf hugo_latest.tar.gz -C /usr/local/bin
 rm /usr/local/bin/README.md
