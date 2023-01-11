21525ad0df28beb9b42d6479ea1d694436e72285
gasstin <gaston_l1994@gmail.com>
2023-01-11 18:19:44 +0000

    fixing setup hugo 2.0

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 # install hugo #
 docker pull klakegg/hugo
+docker run klakegg/hugo
 # install golangcli #
 docker run --rm -v $(pwd):/app -v ~/.cache/golangci-lint/v1.50.1:/root/.cache -w /app golangci/golangci-lint:v1.50.1 golangci-lint run -v
 # install make #
