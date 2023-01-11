bf1c283f7df2dd93c1377f1af5a0c6520c0fd1da
gasstin <gaston_l1994@gmail.com>
2023-01-11 18:10:23 +0000

    fixing setup hugo

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,11 +1,6 @@
 sudo apt-get update
 # install hugo #
-url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
-curl -s $url -L -o hugo.tar.gz
-tar -zxf hugo.tar.gz -C /usr/local/bin
-rm /usr/local/bin/README.md
-rm /usr/local/bin/LICENSE
-rm hugo.tar.gz
+docker pull klakegg/hugo
 # install golangcli #
 docker run --rm -v $(pwd):/app -v ~/.cache/golangci-lint/v1.50.1:/root/.cache -w /app golangci/golangci-lint:v1.50.1 golangci-lint run -v
 # install make #
