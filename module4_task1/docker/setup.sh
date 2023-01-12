330f164fe720afc041fc225a1c2375a59577f50a
gasstin <gaston_l1994@gmail.com>
2023-01-12 20:09:13 +0000

    remove sudo from setup.sh

--- module4_task1/docker/setup.sh
+++ module4_task1/docker/setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get update
+apt-get update
 # install hugo #
 url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
 curl -s $url -L -o hugo.tar.gz
@@ -7,11 +7,9 @@ rm /usr/local/bin/README.md
 rm /usr/local/bin/LICENSE
 # rm hugo.tar.gz
 # install make #
-sudo apt-get install -y make
-sudo npm install -g markdownlint-cli
+apt-get install -y make
+npm install -g markdownlint-cli
 # remove go
-which go
-sudo rm -rf /usr/local/go/bin/go
-sudo rm -rf /usr/bin/go
-which hugo
+rm -rf /usr/local/go/bin/go
+rm -rf /usr/bin/go
 exit 0
