5e985dc4b624e14865a493fdd75a0fe6212f8fe6
gasstin <gaston_l1994@gmail.com>
2023-01-12 13:57:35 +0000

    update setup and Make

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,17 +1,17 @@
 sudo apt-get update
 # install hugo #
-url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
-curl -s $url -L -o hugo.tar.gz
-tar -zxf hugo.tar.gz -C /usr/local/bin
-rm /usr/local/bin/README.md
-rm /usr/local/bin/LICENSE
-rm hugo.tar.gz
+# url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
+# curl -s $url -L -o hugo.tar.gz
+# tar -zxf hugo.tar.gz -C /usr/local/bin
+# rm /usr/local/bin/README.md
+# rm /usr/local/bin/LICENSE
+# rm hugo.tar.gz
 # install make #
 sudo apt-get install -y make
 sudo npm install -g markdownlint-cli
 which go
-sudo rm -rf /usr/bin/go
-which go
-docker version | grep -q Server && echo OK
-which hugo
+# sudo rm -rf /usr/bin/go
+# which go
+# docker version | grep -q Server && echo OK
+# which hugo
 exit 0
