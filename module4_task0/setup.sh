33e4847a0da1d66a158eac4214a4c5c46c9442da
gasstin <gaston_l1994@gmail.com>
2023-01-12 13:45:58 +0000

    updating setup.sh rmv go again

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -10,7 +10,8 @@ rm hugo.tar.gz
 sudo apt-get install -y make
 sudo npm install -g markdownlint-cli
 which go
-rm /usr/bin/go
+rm -rf /usr/bin/go
+which go
 docker version | grep -q Server && echo OK
 which hugo
 exit 0
