40cc50e219c113a7d10a343e445ffe5639fa9f91
gasstin <gaston_l1994@gmail.com>
2023-01-12 13:25:08 +0000

    updating setup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -7,8 +7,6 @@ tar -zxf hugo.tar.gz -C /usr/local/bin
 rm /usr/local/bin/README.md
 rm /usr/local/bin/LICENSE
 rm hugo.tar.gz
-# install golangcli #
-docker run --rm -v $(pwd):/app -v ~/.cache/golangci-lint/v1.50.1:/root/.cache -w /app golangci/golangci-lint:v1.50.1 golangci-lint run -v
 # install make #
 sudo apt-get install -y make
 sudo npm install -g markdownlint-cli
