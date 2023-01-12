13064701c3a751dec119429042281faea2069e70
gasstin <gaston_l1994@gmail.com>
2023-01-12 13:21:53 +0000

    updating Make an setup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,12 @@
 sudo apt-get update
+docker pull golang:1.15.8-buster 
 # install hugo #
-docker pull klakegg/hugo
+url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
+curl -s $url -L -o hugo.tar.gz
+tar -zxf hugo.tar.gz -C /usr/local/bin
+rm /usr/local/bin/README.md
+rm /usr/local/bin/LICENSE
+rm hugo.tar.gz
 # install golangcli #
 docker run --rm -v $(pwd):/app -v ~/.cache/golangci-lint/v1.50.1:/root/.cache -w /app golangci/golangci-lint:v1.50.1 golangci-lint run -v
 # install make #
