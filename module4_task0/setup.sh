c35bf9055ef820e2d6758dc379611ff237d9054f
gasstin <gaston_l1994@gmail.com>
2023-01-12 13:41:04 +0000

    updating setup.sh quite hugo

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,12 +1,11 @@
 sudo apt-get update
-docker pull golang:1.15.8-buster 
 # install hugo #
-url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
-curl -s $url -L -o hugo.tar.gz
-tar -zxf hugo.tar.gz -C /usr/local/bin
-rm /usr/local/bin/README.md
-rm /usr/local/bin/LICENSE
-rm hugo.tar.gz
+# url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz')
+#curl -s $url -L -o hugo.tar.gz
+#tar -zxf hugo.tar.gz -C /usr/local/bin
+#rm /usr/local/bin/README.md
+#rm /usr/local/bin/LICENSE
+#rm hugo.tar.gz
 # install make #
 sudo apt-get install -y make
 sudo npm install -g markdownlint-cli
